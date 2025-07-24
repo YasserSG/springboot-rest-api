903f7f048675031a019c0138975030ac01b3375a
Yasser <yassersuarez007@gmail.com>
2025-07-24 02:39:16 -0600

    Use the most important operations in rest end points

--- src/main/java/com/sjprogramming/restapi/controller/StudentController.java
+++ src/main/java/com/sjprogramming/restapi/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,11 @@ public class StudentController {
 		student.setPercentage(92);
 		repo.save(student);
 		return student;
+	}
+	@DeleteMapping("/student/delete/{id}")
+	public void removeStudent(@PathVariable int id) {
+		Student student = repo.findById(id).get();
+		repo.delete(student);
 	}
 
 }
