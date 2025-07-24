ade882f89a9083b8329099e6932f201c7eb9bee1
Yasser <yassersuarez007@gmail.com>
2025-07-24 01:44:54 -0600

    add third student table in the database

--- src/main/java/com/sjprogramming/restapi/controller/StudentController.java
+++ src/main/java/com/sjprogramming/restapi/controller/StudentController.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable; 
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sjprogramming.restapi.entity.Student;
@@ -24,11 +25,18 @@ public class StudentController {
 		 return students;	
 	}
 	
-	//localhosto:8080/students/1
-	@GetMapping("/student{id}")
+	//localhost:8080/students/1
+	@GetMapping("/students/{id}")
 	public Student getStudent(@PathVariable int id) {
 		Student student = repo.findById(id).get();
 		
 		return student;
+		
 	}
+	@PostMapping("/student/add")
+	public void createStudent(@RequestBody Student student) {
+		repo.save(student);
+	}
+
 }
+
