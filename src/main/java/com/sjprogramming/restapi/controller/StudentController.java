b065550169a0d5ef04f7f166fb4e6d03988e3941
Yasser <yassersuarez007@gmail.com>
2025-07-24 02:26:59 -0600

    creation and testing of rest end points

--- src/main/java/com/sjprogramming/restapi/controller/StudentController.java
+++ src/main/java/com/sjprogramming/restapi/controller/StudentController.java
@@ -3,10 +3,13 @@ package com.sjprogramming.restapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sjprogramming.restapi.entity.Student;
@@ -33,9 +36,21 @@ public class StudentController {
 		return student;
 		
 	}
+	
 	@PostMapping("/student/add")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public void createStudent(@RequestBody Student student) {
 		repo.save(student);
+		
+	}
+	
+	@PutMapping("/student/update/{id}")
+	public Student updateStudents(@PathVariable int id) {
+		Student student = repo.findById(id).get();
+		student.setName("Morty");
+		student.setPercentage(92);
+		repo.save(student);
+		return student;
 	}
 
 }
